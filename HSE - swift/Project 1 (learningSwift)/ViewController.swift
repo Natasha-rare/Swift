8bc6412195778b02381cbbf08f3e8088bd57c812
Natasha-rare <Natasha_ea5@mail.ru>
2020-06-03 15:56:30 +0300

    Second task done (only portrait orientation)

--- HSE - swift/Project 1 (learningSwift)/ViewController.swift
+++ HSE - swift/Project 1 (learningSwift)/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let course: Int = 74
-    var answer: Int = 0
-    var convert:String = ""
+    let dollarCourse: Float = 74
+    var answer: Float = 0
+    var convert: String = ""
+    var order: Bool = true // порядок (доллары -> рубли (true))
     @IBOutlet weak var value: UITextField!
-    
     @IBOutlet weak var converted: UITextField!
+    
+    
+    @IBOutlet weak var ConvertedLabel: UILabel!
+    @IBOutlet weak var ValueLabel: UILabel!
+    
+    
+    @IBAction func ChangeValues(_ sender: UIButton) {
+        order = !order
+        convert = ValueLabel.text ?? "₽"
+        ValueLabel.text = ConvertedLabel.text
+        ConvertedLabel.text = convert
+    }
     
     @IBAction func Run(_ sender: UIButton) {
         convert = value.text ?? "1" // По умолчанию будем конвектировать 1
-        answer = Int(convert)! * course
+        if (order){
+            answer = Float(convert)! * dollarCourse
+        }
+        else{
+            answer = Float(convert)! / dollarCourse
+        }
         converted.text = String(answer)
     }
     
