066009d015895178ec3434f8917819af212729b4
Natasha-rare <Natasha_ea5@mail.ru>
2020-06-04 16:12:13 +0300

    Сделан перевод в рубли/доллары/евро и фунты
    Исправить направляющие (сейчас все съезжает)

--- HSE - swift/Project 1 (learningSwift)/ViewController.swift
+++ HSE - swift/Project 1 (learningSwift)/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     var answer: Float = 0
     var convert: String = ""
     var order: Bool = true // порядок (доллары -> рубли (true))
+    
+    let Course: [String:Float] = ["$": 69, "€": 77, "£": 86, "₽": 1]
+    
     @IBOutlet weak var value: UITextField!
     @IBOutlet weak var converted: UITextField!
     
@@ -22,23 +25,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var ValueLabel: UILabel!
     
     
+    @IBOutlet var Values2: [UIButton]!
+    @IBOutlet var Values1: [UIButton]!
+    
     @IBAction func ChangeValues(_ sender: UIButton) {
         order = !order
-        convert = ValueLabel.text ?? "₽"
+        convert = ValueLabel.text ?? "--"
         ValueLabel.text = ConvertedLabel.text
         ConvertedLabel.text = convert
     }
     
+    
     @IBAction func Run(_ sender: UIButton) {
         convert = value.text ?? "1" // По умолчанию будем конвектировать 1
         if (order){
-            answer = Float(convert)! * dollarCourse
+            answer = Float(convert)! * Course[ValueLabel.text!]! / Course[ConvertedLabel.text!]!
         }
         else{
-            answer = Float(convert)! / dollarCourse
+            answer = Float(convert)! * Course[ValueLabel.text!]! / Course[ConvertedLabel.text!]!
         }
         converted.text = String(answer)
     }
     
+    @IBAction func Value2(_ sender: UIButton) {
+        for button in Values2{
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+            
+        }
+    }
+    
+    @IBAction func Value1(_ sender: UIButton) {
+        for button in Values1{
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+            
+        }
+    }
+    
+    @IBAction func Value1Tap(_ sender: UIButton) {
+        guard let value_to_convert = sender.currentTitle else{
+            return
+        }
+        
+        ValueLabel.text = value_to_convert
+    }
+    
+    @IBAction func ConvertedTap(_ sender: UIButton) {
+        guard let converted_value = sender.currentTitle else{
+            return
+        }
+        
+        ConvertedLabel.text = converted_value
+    }
 }
 
